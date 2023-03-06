a12ed22ee62e261a4401ca7542d4406f6d22ef7f
Nick Benelli <31021342+nick-benelli@users.noreply.github.com>
2023-03-06 15:16:31 -0500

    Update Exploring Housing Data - Pandas.py

--- Exploring Housing Data - Pandas.py
+++ Exploring Housing Data - Pandas.py
@@ -1,8 +1,29 @@
 # Databricks notebook source
 # create dataframes as pandas
-import pandas as pd
-hpi_df = pd.read_csv("HPI_master.csv")
+storage_name = "housingdatastorage"
+container_name = "data-post-etl"
+sas_key = "2FNPZ9FWi4UZ5xJHcmd9J7bh4V2WUG0FIEgqjlZh0ykMy4DOV3s4raCri1HhEjjvD1jnkDx8Wlha+ASttpMp2g=="
 
-# test that dfs loaded
-display(hpi_df.shape) # 121462 rows, 10 columns
-display(hpi_df.head(10))
+storage_account_name = "housingdatastorage"
+storage_account_key = "2FNPZ9FWi4UZ5xJHcmd9J7bh4V2WUG0FIEgqjlZh0ykMy4DOV3s4raCri1HhEjjvD1jnkDx8Wlha+ASttpMp2g=="
+container = "data-post-etl"
+# Configure blob storage account access key globally
+spark.conf.set("fs.azure.account.key.{0}.blob.core.windows.net".format(storage_account_name), storage_account_key)
+
+dbutils.fs.mount(
+ source = "wasbs://{0}@{1}.blob.core.windows.net".format(container, storage_account_name),
+ mount_point = "/mnt/housing",
+ extra_configs = {"fs.azure.account.key.{0}.blob.core.windows.net".format(storage_account_name): storage_account_key}
+)
+
+dbutils.fs.ls("/mnt/housing")
+
+
+
+# COMMAND ----------
+
+spark.conf.set("fs.azure.account.key.housingdatastorage.blob.core.windows.net", "2FNPZ9FWi4UZ5xJHcmd9J7bh4V2WUG0FIEgqjlZh0ykMy4DOV3s4raCri1HhEjjvD1jnkDx8Wlha+ASttpMp2g==")
+df = spark.read.csv("wasbs://housingdatastorage@data-post-etl.blob.core.windows.net/joined_df.csv", header="true")
+
+
+# COMMAND ----------
